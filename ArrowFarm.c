ed1b4e682d6c58d3d232a8a5ff6b3384ba5be0f6
Matthew Huber <unixmonky@gmail.com>
2019-05-14 07:44:09 -0400

    working on simplifying some code, doesn't work yet

---
+++ ArrowFarm.c
@@ -0,0 +1,680 @@
+/*
+Nintendo Switch Fightstick - Proof-of-Concept
+
+Based on the LUFA library's Low-Level Joystick Demo
+	(C) Dean Camera
+Based on the HORI's Pokken Tournament Pro Pad design
+	(C) HORI
+
+This project implements a modified version of HORI's Pokken Tournament Pro Pad
+USB descriptors to allow for the creation of custom controllers for the
+Nintendo Switch. This also works to a limited degree on the PS3.
+
+Since System Update v3.0.0, the Nintendo Switch recognizes the Pokken
+Tournament Pro Pad as a Pro Controller. Physical design limitations prevent
+the Pokken Controller from functioning at the same level as the Pro
+Controller. However, by default most of the descriptors are there, with the
+exception of Home and Capture. Descriptor modification allows us to unlock
+these buttons for our use.
+*/
+
+#include "Joystick.h"
+// #define ALERT_WHEN_DONE
+
+typedef enum
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	H_UP,
+	H_DOWN,
+	H_LEFT,
+	H_RIGHT,
+	X,
+	Y,
+	A,
+	B,
+	L,
+	R,
+	ZL,
+	ZR,
+	PLUS,
+	MINUS,
+	THROW, // Up + A
+	NOTHING,
+	TRIGGERS // L + R together
+} Buttons_t;
+
+typedef struct
+{
+	Buttons_t button;
+	uint16_t duration;
+} command;
+
+static int feed_time = 10; // if you don't upgrade your package capacity
+
+static const command sync_controller[] = {
+	{NOTHING, 250},
+	{TRIGGERS, 5},
+	{NOTHING, 150},
+	{TRIGGERS, 5},
+	{NOTHING, 150},
+	{A, 5},
+	{NOTHING, 250}};
+
+static const command warp[] = {
+	// Warp to island
+	{LEFT,    500},  // Move towards Cliff
+	{ZL,         25},  // Reset View
+	{H_RIGHT,  25},  // Look where we're going
+	{UP,       40},  // Keep Going
+	{A,         25},  // Jump!
+	{X,          5},  // Parasail
+	{UP,      1500},  // Flying
+	{B,          5},   // Fall to our death
+
+};
+
+static const command farm[] = {
+	// Done
+	{H_RIGHT, 50}, // Spin Camera so we know we're done
+	{NOTHING, 50}, //
+	{H_RIGHT, 50}, // Spin Camera so we know we're done
+	{NOTHING, 50}, //
+	{H_RIGHT, 50}, // Spin Camera so we know we're done
+	{NOTHING, 50}, //
+	{H_RIGHT, 50}, // Spin Camera so we know we're done
+	{NOTHING, 50}, //
+};
+
+// Main entry point.
+int main(void)
+{
+	// We'll start by performing hardware and peripheral setup.
+	SetupHardware();
+	// We'll then enable global interrupts for our use.
+	GlobalInterruptEnable();
+	// Once that's done, we'll enter an infinite loop.
+	for (;;)
+	{
+		// We need to run our task to process and deliver data for our IN and OUT endpoints.
+		HID_Task();
+		// We also need to run the main USB management task.
+		USB_USBTask();
+	}
+}
+
+// Configures hardware and peripherals, such as the USB peripherals.
+void SetupHardware(void)
+{
+	// We need to disable watchdog if enabled by bootloader/fuses.
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	// We need to disable clock division before initializing the USB hardware.
+	clock_prescale_set(clock_div_1);
+	// We can then initialize our hardware and peripherals, including the USB stack.
+
+#ifdef ALERT_WHEN_DONE
+// Both PORTD and PORTB will be used for the optional LED flashing and buzzer.
+#warning LED and Buzzer functionality enabled. All pins on both PORTB and \
+PORTD will toggle when printing is done.
+	DDRD = 0xFF; //Teensy uses PORTD
+	PORTD = 0x0;
+	//We'll just flash all pins on both ports since the UNO R3
+	DDRB = 0xFF; //uses PORTB. Micro can use either or, but both give us 2 LEDs
+	PORTB = 0x0; //The ATmega328P on the UNO will be resetting, so unplug it?
+#endif
+	// The USB stack should be initialized last.
+	USB_Init();
+}
+
+// Fired to indicate that the device is enumerating.
+void EVENT_USB_Device_Connect(void)
+{
+	// We can indicate that we're enumerating here (via status LEDs, sound, etc.).
+}
+
+// Fired to indicate that the device is no longer connected to a host.
+void EVENT_USB_Device_Disconnect(void)
+{
+	// We can indicate that our device is not ready (via status LEDs, sound, etc.).
+}
+
+// Fired when the host set the current configuration of the USB device after enumeration.
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+	bool ConfigSuccess = true;
+
+	// We setup the HID report endpoints.
+	ConfigSuccess &= Endpoint_ConfigureEndpoint(JOYSTICK_OUT_EPADDR, EP_TYPE_INTERRUPT, JOYSTICK_EPSIZE, 1);
+	ConfigSuccess &= Endpoint_ConfigureEndpoint(JOYSTICK_IN_EPADDR, EP_TYPE_INTERRUPT, JOYSTICK_EPSIZE, 1);
+
+	// We can read ConfigSuccess to indicate a success or failure at this point.
+}
+
+// Process control requests sent to the device from the USB host.
+void EVENT_USB_Device_ControlRequest(void)
+{
+	// We can handle two control requests: a GetReport and a SetReport.
+
+	// Not used here, it looks like we don't receive control request from the Switch.
+}
+
+// Process and deliver data from IN and OUT endpoints.
+void HID_Task(void)
+{
+	// If the device isn't connected and properly configured, we can't do anything here.
+	if (USB_DeviceState != DEVICE_STATE_Configured)
+		return;
+
+	// We'll start with the OUT endpoint.
+	Endpoint_SelectEndpoint(JOYSTICK_OUT_EPADDR);
+	// We'll check to see if we received something on the OUT endpoint.
+	if (Endpoint_IsOUTReceived())
+	{
+		// If we did, and the packet has data, we'll react to it.
+		if (Endpoint_IsReadWriteAllowed())
+		{
+			// We'll create a place to store our data received from the host.
+			USB_JoystickReport_Output_t JoystickOutputData;
+			// We'll then take in that data, setting it up in our storage.
+			while (Endpoint_Read_Stream_LE(&JoystickOutputData, sizeof(JoystickOutputData), NULL) != ENDPOINT_RWSTREAM_NoError)
+				;
+			// At this point, we can react to this data.
+
+			// However, since we're not doing anything with this data, we abandon it.
+		}
+		// Regardless of whether we reacted to the data, we acknowledge an OUT packet on this endpoint.
+		Endpoint_ClearOUT();
+	}
+
+	// We'll then move on to the IN endpoint.
+	Endpoint_SelectEndpoint(JOYSTICK_IN_EPADDR);
+	// We first check to see if the host is ready to accept data.
+	if (Endpoint_IsINReady())
+	{
+		// We'll create an empty report.
+		USB_JoystickReport_Input_t JoystickInputData;
+		// We'll then populate this report with what we want to send to the host.
+		GetNextReport(&JoystickInputData);
+		// Once populated, we can output this data to the host. We do this by first writing the data to the control stream.
+		while (Endpoint_Write_Stream_LE(&JoystickInputData, sizeof(JoystickInputData), NULL) != ENDPOINT_RWSTREAM_NoError)
+			;
+		// We then send an IN packet on this endpoint.
+		Endpoint_ClearIN();
+	}
+}
+
+typedef enum
+{
+	SYNC_CONTROLLER,
+	WARP,
+	FARM,
+	DONE
+} State_t;
+State_t state = SYNC_CONTROLLER;
+
+USB_JoystickReport_Input_t last_report;
+
+int report_count = 0;
+int bufindex = 0;
+int duration_count = 0;
+int portsval = 0;
+
+void DoControllerInput(char const Button, USB_JoystickReport_Input_t *const ReportData)
+{
+	switch (Button)
+	{
+		case UP:
+			ReportData->LY = STICK_MIN;				
+			break;
+
+		case LEFT:
+			ReportData->LX = STICK_MIN;				
+			break;
+
+		case DOWN:
+			ReportData->LY = STICK_MAX;				
+			break;
+
+		case RIGHT:
+			ReportData->LX = STICK_MAX;				
+			break;
+
+		case H_UP:
+			ReportData->RY = STICK_MIN;				
+			break;
+
+		case H_LEFT:
+			ReportData->RX = STICK_MIN;				
+			break;
+
+		case H_DOWN:
+			ReportData->RY = STICK_MAX;				
+			break;
+
+		case H_RIGHT:
+			ReportData->RX = STICK_MAX;				
+			break;
+
+		case A:
+			ReportData->Button |= SWITCH_A;
+			break;
+
+		case B:
+			ReportData->Button |= SWITCH_B;
+			break;
+
+		case X:
+			ReportData->Button |= SWITCH_X;
+			break;
+
+		case Y:
+			ReportData->Button |= SWITCH_Y;
+			break;
+
+		case R:
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case L:
+			ReportData->Button |= SWITCH_L;
+			break;
+
+		case ZR:
+			ReportData->Button |= SWITCH_ZR;
+			break;
+
+		case ZL:
+			ReportData->Button |= SWITCH_ZL;
+			break;
+
+		case MINUS:
+			ReportData->Button |= SWITCH_MINUS;
+			break;
+
+		case PLUS:
+			ReportData->Button |= SWITCH_PLUS;
+			break;
+
+		case THROW:
+			ReportData->LY = STICK_MIN;				
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case TRIGGERS:
+			ReportData->Button |= SWITCH_L | SWITCH_R;
+			break;
+
+		case NOTHING:
+			ReportData->LX = STICK_CENTER;
+			ReportData->LY = STICK_CENTER;
+			ReportData->RX = STICK_CENTER;
+			ReportData->RY = STICK_CENTER;
+			ReportData->HAT = HAT_CENTER;
+			break;
+	}
+}
+
+// Prepare the next report for the host.
+void GetNextReport(USB_JoystickReport_Input_t *const ReportData)
+{
+
+	// Prepare an empty report
+	memset(ReportData, 0, sizeof(USB_JoystickReport_Input_t));
+	ReportData->LX = STICK_CENTER;
+	ReportData->LY = STICK_CENTER;
+	ReportData->RX = STICK_CENTER;
+	ReportData->RY = STICK_CENTER;
+	ReportData->HAT = HAT_CENTER;
+
+	// States and moves management
+	switch (state)
+	{
+
+	case SYNC_CONTROLLER:
+
+		switch (sync_controller[bufindex].button)
+		{
+		// case UP:
+		// 	ReportData->LY = STICK_MIN;				
+		// 	break;
+
+		// case LEFT:
+		// 	ReportData->LX = STICK_MIN;				
+		// 	break;
+
+		// case DOWN:
+		// 	ReportData->LY = STICK_MAX;				
+		// 	break;
+
+		// case RIGHT:
+		// 	ReportData->LX = STICK_MAX;				
+		// 	break;
+
+		// case H_UP:
+		// 	ReportData->RY = STICK_MIN;				
+		// 	break;
+
+		// case H_LEFT:
+		// 	ReportData->RX = STICK_MIN;				
+		// 	break;
+
+		// case H_DOWN:
+		// 	ReportData->RY = STICK_MAX;				
+		// 	break;
+
+		// case H_RIGHT:
+		// 	ReportData->RX = STICK_MAX;				
+		// 	break;
+
+		// case A:
+		// 	ReportData->Button |= SWITCH_A;
+		// 	break;
+
+		// case B:
+		// 	ReportData->Button |= SWITCH_B;
+		// 	break;
+
+		// case X:
+		// 	ReportData->Button |= SWITCH_X;
+		// 	break;
+
+		// case Y:
+		// 	ReportData->Button |= SWITCH_Y;
+		// 	break;
+
+		// case R:
+		// 	ReportData->Button |= SWITCH_R;
+		// 	break;
+
+		// case L:
+		// 	ReportData->Button |= SWITCH_L;
+		// 	break;
+
+		// case ZR:
+		// 	ReportData->Button |= SWITCH_ZR;
+		// 	break;
+
+		// case ZL:
+		// 	ReportData->Button |= SWITCH_ZL;
+		// 	break;
+
+		// case MINUS:
+		// 	ReportData->Button |= SWITCH_MINUS;
+		// 	break;
+
+		// case PLUS:
+		// 	ReportData->Button |= SWITCH_PLUS;
+		// 	break;
+
+		// case THROW:
+		// 	ReportData->LY = STICK_MIN;				
+		// 	ReportData->Button |= SWITCH_R;
+		// 	break;
+
+		// case TRIGGERS:
+		// 	ReportData->Button |= SWITCH_L | SWITCH_R;
+		// 	break;
+
+		// case NOTHING:
+		// 	ReportData->LX = STICK_CENTER;
+		// 	ReportData->LY = STICK_CENTER;
+		// 	ReportData->RX = STICK_CENTER;
+		// 	ReportData->RY = STICK_CENTER;
+		// 	ReportData->HAT = HAT_CENTER;
+		// 	break;
+		// }
+		duration_count++;
+
+		if (duration_count > sync_controller[bufindex].duration)
+		{
+			bufindex++;
+			duration_count = 0;
+		}
+		if (bufindex > (int)(sizeof(sync_controller) / sizeof(sync_controller[0]) - 1))
+		{
+			portsval = ~portsval;
+			PORTD = portsval; //flash LED(s) and sound buzzer if attached
+			PORTB = portsval;
+			state = WARP;
+			bufindex = 0;
+		}
+		break;
+
+// Start at Korgu Chideh Shrine
+	case WARP:
+
+		switch (warp[bufindex].button)
+		{
+		case UP:
+			ReportData->LY = STICK_MIN;				
+			break;
+
+		case LEFT:
+			ReportData->LX = STICK_MIN;				
+			break;
+
+		case DOWN:
+			ReportData->LY = STICK_MAX;				
+			break;
+
+		case RIGHT:
+			ReportData->LX = STICK_MAX;				
+			break;
+
+		case H_UP:
+			ReportData->RY = STICK_MIN;				
+			break;
+
+		case H_LEFT:
+			ReportData->RX = STICK_MIN;				
+			break;
+
+		case H_DOWN:
+			ReportData->RY = STICK_MAX;				
+			break;
+
+		case H_RIGHT:
+			ReportData->RX = STICK_MAX;				
+			break;
+
+		case A:
+			ReportData->Button |= SWITCH_A;
+			break;
+
+		case B:
+			ReportData->Button |= SWITCH_B;
+			break;
+
+		case X:
+			ReportData->Button |= SWITCH_X;
+			break;
+
+		case Y:
+			ReportData->Button |= SWITCH_Y;
+			break;
+
+		case R:
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case L:
+			ReportData->Button |= SWITCH_L;
+			break;
+
+		case ZR:
+			ReportData->Button |= SWITCH_ZR;
+			break;
+
+		case ZL:
+			ReportData->Button |= SWITCH_ZL;
+			break;
+
+		case MINUS:
+			ReportData->Button |= SWITCH_MINUS;
+			break;
+
+		case PLUS:
+			ReportData->Button |= SWITCH_PLUS;
+			break;
+
+		case THROW:
+			ReportData->LY = STICK_MIN;				
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case TRIGGERS:
+			ReportData->Button |= SWITCH_L | SWITCH_R;
+			break;
+
+		case NOTHING:
+			ReportData->LX = STICK_CENTER;
+			ReportData->LY = STICK_CENTER;
+			ReportData->RX = STICK_CENTER;
+			ReportData->RY = STICK_CENTER;
+			ReportData->HAT = HAT_CENTER;
+			break;
+		}
+
+		duration_count++;
+
+		if (duration_count > warp[bufindex].duration)
+		{
+			bufindex++;
+			duration_count = 0;
+		}
+
+		if (bufindex > (int)(sizeof(warp) / sizeof(warp[0])) - 1)
+		{
+			portsval = ~portsval;
+			PORTD = portsval; //flash LED(s) and sound buzzer if attached
+			PORTB = portsval;
+			state = FARM;
+			bufindex = 0;
+		}
+
+		break;
+
+	case FARM:
+
+		switch (farm[bufindex].button)
+		{
+
+		case UP:
+			ReportData->LY = STICK_MIN;				
+			break;
+
+		case LEFT:
+			ReportData->LX = STICK_MIN;				
+			break;
+
+		case DOWN:
+			ReportData->LY = STICK_MAX;				
+			break;
+
+		case RIGHT:
+			ReportData->LX = STICK_MAX;				
+			break;
+
+		case H_UP:
+			ReportData->RY = STICK_MIN;				
+			break;
+
+		case H_LEFT:
+			ReportData->RX = STICK_MIN;				
+			break;
+
+		case H_DOWN:
+			ReportData->RY = STICK_MAX;				
+			break;
+
+		case H_RIGHT:
+			ReportData->RX = STICK_MAX;				
+			break;
+
+		case A:
+			ReportData->Button |= SWITCH_A;
+			break;
+
+		case B:
+			ReportData->Button |= SWITCH_B;
+			break;
+
+		case X:
+			ReportData->Button |= SWITCH_X;
+			break;
+
+		case Y:
+			ReportData->Button |= SWITCH_Y;
+			break;
+
+		case R:
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case L:
+			ReportData->Button |= SWITCH_L;
+			break;
+
+		case ZR:
+			ReportData->Button |= SWITCH_ZR;
+			break;
+
+		case ZL:
+			ReportData->Button |= SWITCH_ZL;
+			break;
+
+		case MINUS:
+			ReportData->Button |= SWITCH_MINUS;
+			break;
+
+		case PLUS:
+			ReportData->Button |= SWITCH_PLUS;
+			break;
+
+		case THROW:
+			ReportData->LY = STICK_MIN;				
+			ReportData->Button |= SWITCH_R;
+			break;
+
+		case TRIGGERS:
+			ReportData->Button |= SWITCH_L | SWITCH_R;
+			break;
+
+		case NOTHING:
+			ReportData->LX = STICK_CENTER;
+			ReportData->LY = STICK_CENTER;
+			ReportData->RX = STICK_CENTER;
+			ReportData->RY = STICK_CENTER;
+			ReportData->HAT = HAT_CENTER;
+			break;
+		}
+		duration_count++;
+
+		if (duration_count > farm[bufindex].duration)
+		{
+			bufindex++;
+			duration_count = 0;
+		}
+
+		if (bufindex > (int)(sizeof(farm) / sizeof(farm[0])) - 1)
+		{
+			bufindex = 0;
+			feed_time--;
+			if (feed_time < 1)
+			{
+				portsval = ~portsval;
+				PORTD = portsval; //flash LED(s) and sound buzzer if attached
+				PORTB = portsval;
+				state = DONE;
+				feed_time = 10;
+			}
+		}
+
+		break;
+	}
+	return;
+}
